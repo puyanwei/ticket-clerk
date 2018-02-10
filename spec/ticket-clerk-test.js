60eb5615f4ea92261e69f5e339982e376275a276
Puyan Wei <puyanwei@gmail.com>
2018-02-10 18:59:33 +0000

    refactored, wrote tests for giveChange and canGiveThreeTwentyFives and canGiveFiftyTwentyFive

--- spec/ticket-clerk-test.js
+++ spec/ticket-clerk-test.js
@@ -2,40 +2,83 @@ describe("#TicketClerk", function() {
   beforeEach(function() {
     ticketClerk = new TicketClerk();
   });
-  describe("#entry", function() {
-    it("shows a hash of the money in the till, which starts with zero of each denominator", function() {
-      expect(ticketClerk.till).toEqual({ 25: 0, 50: 0, 100: 0 });
+  // describe("#sell", function() {
+  //   it("shows a hash of the money in the till, which starts with zero of each denominator", function() {
+  //     expect(ticketClerk.till).toEqual({ 25: 0, 50: 0, 100: 0 });
+  //   });
+  //   it("entering 25, 25, 50 should end up with one 25 and one 50 in the till", function() {
+  //     ticketClerk.sell([25, 25, 50]);
+  //     expect(ticketClerk.till).toEqual({ 25: 1, 50: 1, 100: 0 });
+  // });
+  // it("entering 25, 25, 50 should return 'YES' as it can return change", function() {
+  //   expect(ticketClerk.sell([25, 25, 50])).toEqual("YES");
+  // });
+  // });
+
+  describe("#canGiveFiftyTwentyFive", function() {
+    it("returns true if there are more then one fifty and one twenty-five", function() {
+      ticketClerk.till = { 25: 3, 50: 1, 100: 0 };
+      expect(ticketClerk.canGiveFiftyTwentyFive()).toBe(true);
     });
-    it("entering 25, 25, 50 should end up with one 25 and one 50 in the till", function() {
-      ticketClerk.entry([25, 25, 50]);
-      expect(ticketClerk.till).toEqual({ 25: 1, 50: 1, 100: 0 });
+    it("returns false if there are not more then one fifty and one twenty-five", function() {
+      ticketClerk.till = { 25: 3, 50: 0, 100: 0 };
+      expect(ticketClerk.canGiveFiftyTwentyFive()).toBe(false);
     });
-    // it("entering 25, 25, 50 should return 'YES' as it can return change", function() {
-    //   expect(ticketClerk.entry([25, 25, 50])).toEqual("YES");
-    // });
+    it("returns false if there are not more then one fifty and one twenty-five", function() {
+      ticketClerk.till = { 25: 0, 50: 1, 100: 0 };
+      expect(ticketClerk.canGiveFiftyTwentyFive()).toBe(false);
+    });
   });
 
-  describe("#fiftyNote", function() {
-    it("giving 50 deducts a 25 note and adds in a 50 note", function() {
-      ticketClerk.till = { 25: 1, 50: 0, 100: 0 };
-      ticketClerk.fiftyNote();
-      expect(ticketClerk.till).toEqual({ 25: 0, 50: 1, 100: 0 });
+  describe("#canGiveThreeTwentyFives", function() {
+    it("returns true if there are more then two twenty-fives", function() {
+      ticketClerk.till = { 25: 3, 50: 1, 100: 0 };
+      expect(ticketClerk.canGiveThreeTwentyFives()).toBe(true);
+    });
+    it("returns false if there are not more then two twenty-fives", function() {
+      ticketClerk.till = { 25: 2, 50: 1, 100: 0 };
+      expect(ticketClerk.canGiveThreeTwentyFives()).toBe(false);
     });
   });
 
   describe("#hundredNote", function() {
-    it("entering 100 with nothing in the till returns 'NO'", function() {
-      expect(ticketClerk.hundredNote()).toEqual("NO");
-    });
+    // it("entering 100 with nothing in the till returns 'NO'", function() {
+    //   expect(ticketClerk.hundredNote()).toEqual("NO");
+    // });
     it("giving 100 deducts a 50 note and a 25 note, and adds in a 100 note", function() {
       ticketClerk.till = { 25: 2, 50: 2, 100: 0 };
       ticketClerk.hundredNote();
       expect(ticketClerk.till).toEqual({ 25: 1, 50: 1, 100: 1 });
     });
+  });
+
+  describe("#hundredNoteTwenties", function() {
     it("giving 100 deducts a three 25 notes, and adds in a 100 note", function() {
       ticketClerk.till = { 25: 6, 50: 0, 100: 0 };
-      ticketClerk.hundredNote();
+      ticketClerk.hundredNoteTwenties();
       expect(ticketClerk.till).toEqual({ 25: 3, 50: 0, 100: 1 });
+    });
+  });
+
+  describe("#giveChange", function() {
+    it("adds a 25 note to the till", function() {
+      ticketClerk.giveChange(25);
+      expect(ticketClerk.till).toEqual({ 25: 1, 50: 0, 100: 0 });
+    });
+    it("adds a 50 to the till, and deducts a 25 note", function() {
+      ticketClerk.till = { 25: 3, 50: 0, 100: 0 };
+      ticketClerk.giveChange(50);
+      expect(ticketClerk.till).toEqual({ 25: 2, 50: 1, 100: 0 });
+    });
+    it("adds a 100 to the till, and deducts a fifty and a twenty-five note", function() {
+      ticketClerk.till = { 25: 2, 50: 1, 100: 0 };
+      ticketClerk.giveChange(100);
+      expect(ticketClerk.till).toEqual({ 25: 1, 50: 0, 100: 1 });
+    });
+    it("adds a 100 to the till, and deducts three twenty-five notes", function() {
+      ticketClerk.till = { 25: 4, 50: 0, 100: 0 };
+      ticketClerk.giveChange(100);
+      expect(ticketClerk.till).toEqual({ 25: 1, 50: 0, 100: 1 });
     });
   });
 });
--- src/ticket-clerk.js
+++ src/ticket-clerk.js
@@ -2,38 +2,50 @@ var TicketClerk = function() {
   this.till = { 25: 0, 50: 0, 100: 0 };
 };
 
-TicketClerk.prototype.entry = function(array) {
+TicketClerk.prototype.sell = function(array) {
   array.forEach(note => {
-    if (note === 25) {
-      this.till[25]++;
-    }
-    if (note === 50) {
-      if (this.till[25] < 1) {
-        return "NO";
-      }
-      this.fiftyNote();
-    }
-    if (note === 100) {
-      return this.hundredNote();
+    if (this.canMakeChange()) {
+      this.giveChange(note);
+    } else {
+      return "NO";
     }
     return "YES";
   });
 };
 
-TicketClerk.prototype.fiftyNote = function() {
-  this.till[25]--;
-  this.till[50]++;
+TicketClerk.prototype.canMakeChange = function(note) {
+  if (note === 100) {
+    return this.canGiveFiftyTwentyFive() || this.canGiveThreeTwentyFives();
+  }
+  if (note === 50) {
+    return this.till[25] > 0;
+  }
+  return true;
 };
 
-TicketClerk.prototype.hundredNote = function() {
-  if (this.till[25] > 0 && this.till[50] > 0) {
+TicketClerk.prototype.giveChange = function(note) {
+  if (note === 25) {
+    this.till[25]++;
+  }
+  if (note === 50) {
+    this.till[25]--;
+    this.till[50]++;
+  }
+  if (note === 100 && this.canGiveFiftyTwentyFive()) {
     this.till[25]--;
     this.till[50]--;
     this.till[100]++;
   }
-  if (this.till[25] > 2) {
+  if (note === 100 && this.canGiveThreeTwentyFives()) {
     this.till[25] -= 3;
     this.till[100]++;
   }
-  return "NO";
+};
+
+TicketClerk.prototype.canGiveThreeTwentyFives = function() {
+  return this.till[25] > 2;
+};
+
+TicketClerk.prototype.canGiveFiftyTwentyFive = function() {
+  return this.till[25] > 0 && this.till[50] > 0;
 };
